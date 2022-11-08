e8d0e00a2d15dee382351670f1affa9b930991e5
fabriziorosales <fabrizio282002@gmail.com>
2022-11-08 17:47:41 +0100

    pagina res

---
+++ js/estilos.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', () => {
+
+    const tipos = document.getElementsByClassName('tipo');
+    console.log(tipos.length);
+    var tsala = '';
+    var nsala = '';
+    for (let i = 0; i < tipos.length; i++) {
+        nsala = tipos[i].getElementsByClassName('salas')[0].className.split(' ')[1];
+        console.log(nsala);
+        var csala = tipos[i].getElementsByClassName(nsala);
+        console.log(csala.length);
+        for (let j = 0; j < csala.length; j++) {
+
+            tsala = csala[j];
+            console.log(tsala);
+            tsala.onclick = () => {
+                console.log(tsala);
+                tsala.classList.toggle('mostrar');
+                // tsala.getElementsByTagName('h3')[0].classList.toggle('info-salas');
+                // tsala.getElementsByClassName('info-salas')[0].classList.toggle('mostrar');
+                // for (let j = 0; j < tsala.getElementsByClassName('salas').length; j++) {
+                //     tsala.getElementsByClassName('salas')[j].classList.toggle(nsala);
+                // }
+            }
+        }
+        // tsala.onmouseout = () => {
+        //     tsala.classList.remove('remove')
+        //     for (let j = 0; j < tsala.getElementsByClassName('salas').length; j++) {
+        //         tsala.getElementsByClassName('salas')[j].classList.remove(nsala);
+        //     }
+        // }
+    }
+
+    // const tipos2 = document.getElementsByClassName('tipo-sal');
+    // for (let i = 0; i < tipos2.length; i++) {
+    //     let nsala = tipos2[i].className.split(' ')[1]
+    //     // console.log(nsala);
+    //     let tsala = document.getElementsByClassName(nsala)[0]
+    //     tsala.onclick = () => {
+    //         // tsala.classList.add('remove')
+    //         // console.log(document.getElementsByClassName('modal')[i].className == 'modal '+nsala);
+    //         if (document.getElementsByClassName('modal')[i].className == 'modal '+nsala) {
+    //             // for (let j = 0; j < tsala.getElementsByClassName('salas').length; j++) {
+    //             //     tsala.getElementsByClassName('salas')[j].classList.add(nsala);
+    //             // }
+    //             // console
+    //             document.getElementsByClassName('modal')[i].classList.add('mostrar');
+    //         }
+    //     }
+        // tsala.onclick = () => {
+        //     tsala.classList.remove('remove')
+        //     for (let j = 0; j < tsala.getElementsByClassName('salas').length; j++) {
+        //         tsala.getElementsByClassName('salas')[j].classList.remove(nsala);
+        //     }
+        // }
+    // }
+})
--- view/restaurante.php
+++ view/restaurante.php
@@ -4,9 +4,95 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/estilos.js"></script>
     <title>Restaurante</title>
 </head>
 <body>
-    
+    <div class="background">
+        <div class="contenido restaurante">
+            <!-- <div class="recuadro"> -->
+                <!-- Mostrar todos los sitios/salas -->
+                <div class="tipo">
+                    <div class="salas terraza">
+                        <div class="blur">
+                            <h3>Terraza 1</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="salas terraza">
+                        <div class="blur">
+                            <h3>Terraza 2</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="tipo">
+                    <div class="salas comedor">
+                        <div class="blur">
+                            <h3>Comedor 1</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="salas comedor">
+                        <div class="blur">
+                            <h3>Comedor 2</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="tipo">
+                    <div class="salas privada">
+                        <div class="blur">
+                            <h3>Privado 1</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas totales: 5</p>
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="salas privada">
+                        <div class="blur">
+                            <h3>Privado 2</h3>
+                            <!-- Mostrar las mesas disponibles -->
+                            <div class="info-salas">
+                                <p>Mesas disponibles: 0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+                
+
+                
+
+                   
+            <!-- </div> -->
+        </div>
+
+        <div class="color-back ">
+            <div class="modal">
+                
+            </div>
+        </div>
+    </div>
 </body>
 </html>
