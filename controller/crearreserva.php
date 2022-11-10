b543fbde7eac21910e8a95d569e7984ddf69d2fd
fabriziorosales <fabrizio282002@gmail.com>
2022-11-10 16:54:36 +0100

    todo 10/11/2022

--- controller/crearreserva.php
+++ controller/crearreserva.php
@@ -1,13 +1,12 @@
 <?php
-
+session_start();
    
 require_once '../model/reserva.php';
-$id_usuario=$_SESSION['user'];
-$id_sala=$_SESSION['sala'];
+$correo=$_SESSION['user'];
 $id_mesa = $_POST['mesa']; 
 $nombre_reserva = $_POST['nombre_reserva'];
 
-Reserva::crearReserva($nombre_reserva, $id_usuario, $id_sala, $id_mesa);
+Reserva::crearReserva($correo,$nombre_reserva, $id_mesa);
 
 echo"<script>window.location.href = '../view/sala.php' </script>";
 
