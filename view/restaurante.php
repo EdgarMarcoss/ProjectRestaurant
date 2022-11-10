f7dfa8169943d85c41b4b524a9364bd8c15cc5a8
fabriziorosales <fabrizio282002@gmail.com>
2022-11-10 18:05:27 +0100

    arreglos salas 10/11/2022

--- view/restaurante.php
+++ view/restaurante.php
@@ -28,14 +28,20 @@ if (!isset($_SESSION['user'])) {
             $vuelta = 0;
             
             foreach (Sala::getSala() as $element) {
-                // var_dump(explode(" ", $element["nombre_sala"])[0]);
                 echo '<div class="content">';
                 echo '<div class="salas '.explode("_", $element["nombre_sala"])[0].'">
                             <div class="blur">
                                 <h3>'.str_replace("_", " ", $element["nombre_sala"]).'</h3>
                                 <div class="info-salas">
                                     <p>Mesas totales: '.$element["Mid"].'</p>
-                                    <p>Mesas disponibles: '.Sala::getMesaLibre()[$vuelta]["Mid"].'</p>
+                                    <p>Mesas disponibles: ';
+                                    if (Sala::getMesaLibre()[$vuelta]["nombre_sala"] == $element["nombre_sala"]) {
+                                        $disponible = Sala::getMesaLibre()[$vuelta]["Mid"];
+                                        $vuelta++;
+                                    } else {
+                                        $disponible = "0";
+                                    }
+                                    echo $disponible.'</p>
                                     <form action="sala.php" method="post" class="ver">
                                         <input type="hidden" name="sala" value="'.$element['id'].'">
                                         <button type="submit" class="btn-salas">Ver</button>
@@ -45,9 +51,8 @@ if (!isset($_SESSION['user'])) {
                         </div>';
                 echo '</div>';
 
-                        $vuelta++;
-            } ?> 
-            <a href="vista.php"><button>Reservas</button></a>  
+            } ?>
+            <a class="btn-reservas" href="vista.php"><button>Reservas</button></a>  
         </div>
         <div class="color-back ">
             <div class="modal">
