e40fcd37ef8c7ca93d5b7aeaa0be5121c110904d
fabriziorosales <fabrizio282002@gmail.com>
2022-11-10 17:03:29 +0100

    mobi 10/11/2022

--- img/mesas/bd_restaurante.sql
+++ img/mesas/bd_restaurante.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-11-2022 a las 16:55:38
+-- Tiempo de generación: 10-11-2022 a las 16:59:38
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
